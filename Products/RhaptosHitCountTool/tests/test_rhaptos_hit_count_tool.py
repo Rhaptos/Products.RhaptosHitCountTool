ece7c6b7e3c1d5acfe42b2440b45cf235be5ee2b
Raj Shah <raj@cnx.rice.edu>
2009-10-23 17:28:53 +0000

    got unit tests working for RhaptosHitCountTool

--- Products/RhaptosHitCountTool/tests/test_rhaptos_hit_count_tool.py
+++ Products/RhaptosHitCountTool/tests/test_rhaptos_hit_count_tool.py
@@ -26,17 +26,18 @@ $Id: $
 
 import Products.RhaptosHitCountTool
 
-from Products.RhaptosTest import base
+from Products.RhaptosTest.base import RhaptosTestCase
 
 
-base.PRODUCTS_TO_LOAD_ZCML = [('configure.zcml', Products.RhaptosHitCountTool),]
-base.PRODUCTS_TO_INSTALL = ['Products.RhaptosHitCountTool',]
+class TestRhaptosHitCountTool(RhaptosTestCase):
 
+    products_to_load_zcml = [('configure.zcml', Products.RhaptosHitCountTool),]
 
-class TestRhaptosHitCountTool(base.RhaptosTestCase):
+    def setUp(self):
+        RhaptosTestCase.setUp(self)
 
-    def test_pass(self):
-        assert 1 == 1
+    def test_hit_count_tool(self):
+        self.assertEqual(1, 1)
 
 
 def test_suite():
