e666eb60bb21ed1afeefa319a0447800c54cdd27
Ross J. Reedstrom <reedstrm@cnx.org>
2011-02-01 15:45:10 +0000

    [releaser] preparing release 0.12

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.11'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = read("Products", "RhaptosHitCountTool", "version.txt").strip()
 
 setup(name='Products.RhaptosHitCountTool',
       version=version,
